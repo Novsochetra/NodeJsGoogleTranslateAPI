d5063dc7981633ad4a1a4975ccc66f046e1e6443
Novsochetra <sochetra12.nov@gmail.com>
2020-08-25 16:29:28 +0700

    update script

--- server.ts
+++ server.ts
@@ -22,6 +22,12 @@ mainApp.use(function (req: Request, res: Response): any {
   return render404Page(req, res);
 });
 
+app.listen(config.PORT, (_req: Request, _res: Response): void => {
+  console.log(
+    `Example app listening at http://${config.HOST_NAME}:${config.PORT}`
+  );
+});
+
 const render404Page = (req: Request, res: Response): any => {
   return res.format({
     html: function () {
